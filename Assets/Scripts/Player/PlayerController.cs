bee4d6f307dd43d3dc5b12a3034edb5197637835
JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>
2023-10-30 16:41:46 +0000

    Removed requirement for Rigidbody and changed collision detection
    Movement is now fully calculated without using rigidbody velocity, and collision now uses 'prediction' to determine whether a collision will occur.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,34 +1,27 @@
-using System;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+[RequireComponent(typeof(Collider2D))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private InputReader _inputReader;
     [SerializeField] private LayerMask _playerLayer;
-    [Header("Vertical")]
-    [SerializeField] private float _jumpForce = 25f;
+    [Header("Gravity")]
     [SerializeField] private float _gravity = 100f;
     [SerializeField] private float _maxFallSpeed = 40f;
-    [SerializeField] private float _collisionCheckDistance = 0.05f;
-    [SerializeField] private Transform _groundCheck;
-    [SerializeField] private Transform _ceilingCheck;
-    [Header("Horizontal")]
+    [Header("Movement")]
+    [SerializeField] private float _jumpForce = 25f;
     [SerializeField] private float _maxSpeed = 15;
     [SerializeField] private float _acceleration = 100;
     [SerializeField] private float _deceleration = 50;
 
-    private Rigidbody2D _rigidbody;
     private CapsuleCollider2D _collider;
 
     private Vector2 _velocity = Vector2.zero;
-    private bool _isGrounded = false;
 
     private float _movementInputVector;
 
     private void Awake()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
         _collider = GetComponent<CapsuleCollider2D>();
 
         _inputReader.OnJumpPressed += JumpPressed;
@@ -37,57 +30,54 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        CheckCollisions();
         Gravity();
-
         CalculateHorizontalMovement();
 
-        ApplyMovement();
-    }
+        CheckCollisions();
 
+        ApplyFinalMovement();
+    }
+    
     private void CheckCollisions()
     {
-        bool hitGroundThisFrame = Physics2D.OverlapCircle(_groundCheck.position, _collisionCheckDistance, ~_playerLayer);
-        bool hitCeilingThisFrame = Physics2D.OverlapCircle(_ceilingCheck.position, _collisionCheckDistance, ~_playerLayer);
-
-
-        if (hitCeilingThisFrame)
+        if (Physics2D.OverlapCapsule(new Vector2(transform.position.x, transform.position.y + _velocity.y), _collider.bounds.size, _collider.direction, 0f, ~_playerLayer))
         {
-            _velocity.y = Mathf.Min(0, _velocity.y);
-        }
-
-        if (!_isGrounded && hitGroundThisFrame)
-        {
-            _isGrounded = true;
-            RaycastHit2D hit = Physics2D.Raycast(_collider.bounds.center, Vector2.down, _collider.size.y * 0.5f, ~_playerLayer);
-            if (hit.collider != null)
+            while (!Physics2D.OverlapCapsule(new Vector2(transform.position.x, transform.position.y + Mathf.Sign(_velocity.y)), _collider.bounds.size, _collider.direction, 0f, ~_playerLayer))
             {
-                float distanceSunkInGround = hit.collider.bounds.max.y - _collider.bounds.min.y;
-                Vector2 newPosition = new(_rigidbody.position.x, _rigidbody.position.y + distanceSunkInGround);
-                _rigidbody.position = newPosition;
+                Vector2 newPosition = new(transform.position.x, transform.position.y + Mathf.Sign(_velocity.y));
+                transform.position = newPosition;
             }
+            _velocity.y = 0f;
         }
-        else if (_isGrounded && !hitGroundThisFrame)
+
+        if (Physics2D.OverlapCapsule(new Vector2(transform.position.x + _velocity.x, transform.position.y), _collider.bounds.size, _collider.direction, 0f, ~_playerLayer))
         {
-            _isGrounded = false;
+            while (!Physics2D.OverlapCapsule(new Vector2(transform.position.x + Mathf.Sign(_velocity.x), transform.position.y), _collider.bounds.size, _collider.direction, 0f, ~_playerLayer))
+            {
+                Vector2 newPosition = new(transform.position.x + Mathf.Sign(_velocity.x), transform.position.y);
+                transform.position = newPosition;
+            }
+            _velocity.x = 0f;
         }
+    }
+
+    private bool IsGrounded()
+    {
+        if (Physics2D.OverlapCapsule(new Vector2(transform.position.x, transform.position.y - 0.4f), _collider.bounds.size, _collider.direction, 0f, ~_playerLayer))
+        {
+            return true;
+        }
+        return false;
     }
 
     private void Gravity()
     {
-        if (_isGrounded && _velocity.y <= 0f)
-        {
-            _velocity.y = 0f;
-        }
-        else
-        {
-            _velocity.y = Mathf.MoveTowards(_velocity.y, -_maxFallSpeed, _gravity * Time.fixedDeltaTime);
-        }
+        _velocity.y = Mathf.MoveTowards(_velocity.y, -_maxFallSpeed, _gravity * Time.fixedDeltaTime);
     }
 
     private void JumpPressed()
     {
-        if (_isGrounded)
+        if (IsGrounded())
             _velocity.y = _jumpForce;
     }
 
@@ -103,23 +93,9 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void ApplyMovement()
+    private void ApplyFinalMovement()
     {
-        _rigidbody.velocity = _velocity;
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        if (_groundCheck != null)
-        {
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(_groundCheck.position, _collisionCheckDistance);
-        }
-
-        if (_ceilingCheck != null)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(_ceilingCheck.position, _collisionCheckDistance);
-        }
+        Vector2 newPosition = new(transform.position.x + _velocity.x, transform.position.y + _velocity.y);
+        transform.position = newPosition;
     }
 }
