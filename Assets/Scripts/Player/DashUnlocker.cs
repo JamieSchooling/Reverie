1dd7eb28bd2246c4efd8b9e780891026ba2a20f4
JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>
2023-11-09 20:59:25 +0000

    Added dash tutorial

--- Assets/Scripts/Player/DashUnlocker.cs
+++ Assets/Scripts/Player/DashUnlocker.cs
@@ -1,12 +1,45 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public class DashUnlocker : MonoBehaviour
 {
+    [SerializeField] private InputReader _inputReader;
+    [SerializeField] private ButtonPromptText _dashButtonPrompt;
+
+    private bool _isDashUnlocked = false;
+
+    private void OnEnable()
+    {
+        _inputReader.OnDashPressed += OnDashPressed;
+    }
+
+    private void OnDisable()
+    {
+        _inputReader.OnDashPressed -= OnDashPressed;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent(out PlayerController player))
         {
             player.IsDashUnlocked = true;
+            _isDashUnlocked = true;
+            Time.timeScale = 0f;
+            _inputReader.DisablePauseInput();
+            _dashButtonPrompt.gameObject.SetActive(true);
         }
     }
+
+    private void OnDashPressed()
+    {
+        if (_isDashUnlocked)
+        {
+            Time.timeScale = 1f;
+            _inputReader.EnablePauseInput();
+            _dashButtonPrompt.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+
 }
