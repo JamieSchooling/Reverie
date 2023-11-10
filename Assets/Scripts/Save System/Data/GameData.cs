92e6d5746c6dba5cd0fb3be562b0b66e6d067935
JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>
2023-11-10 18:14:32 +0000

    Added collectibles to game save data

--- Assets/Scripts/Save System/Data/GameData.cs
+++ Assets/Scripts/Save System/Data/GameData.cs
@@ -3,18 +3,18 @@ using UnityEngine;
 [System.Serializable]
 public class GameData
 {
+    public bool isPrologueComplete = false;
     public int deathCount;
     public Vector2 playerRespawnPoint;
     public Vector3 cameraTargetOnReset;
-    public bool isPrologueComplete = false;
-    public bool isDashUnlocked = false;
+    public SerializableDictionary<string, bool> collectibles;
 
     public GameData()
     {
+        isPrologueComplete = false;
         deathCount = 0;
         playerRespawnPoint = Vector2.zero;
         cameraTargetOnReset = Vector3.zero;
-        isPrologueComplete = false;
-        isDashUnlocked = false;
+        collectibles = new SerializableDictionary<string, bool>();
     }
 }
